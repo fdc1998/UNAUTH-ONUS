b5bdef97b3cb369530720eedba00c34781f6ea1d
Flavio Fernandes <fdc1998@gmail.com>
2021-09-20 08:03:36 -0300

    20-09

--- app.py
+++ app.py
@@ -1,62 +1,63 @@
 import json
-from flask_modals import Modal, render_template_modal
 from flask import Flask, redirect, url_for, render_template, request, abort, flash
 from models.user_model import *
 import jsonify
 from playhouse.shortcuts import model_to_dict
+import time
+import urllib.parse
 
 app = Flask(__name__, static_folder='static')
-modal = Modal(app)
+
+
+@app.route("/removeonu", methods=["POST","GET"])
+def target():
+    if request.args:
+        serial = str(request.args['serial'])
+        localidade = str(request.args['olts'])
+        return render_template('loading.html', my_data=[localidade, serial])
+
+
+@app.route("/processing")
+def processing():
+    return render_template('success.html', passed_data="Onu removida com sucesso")
+
 
 @app.route('/')
 def index():
-
-    flash('Invalid username or password', 'danger')
-    return render_template_modal('index.html', modal='modal-form')
-
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-    if request.method == 'POST':
-        if request.form['nome'] == 'admin':
-            return redirect(url_for('sucess'), code=302)
-        else:
-            abort(401)
+    if request.method == 'GET':
+        print('get')
+        localidades = list(Localidade.select().dicts())
+        return render_template('onu-remove.html', my_string="Include Help!", my_list=localidades)
     else:
-        abort(403)
+        print('post')
 
-@app.route('/remove/', methods=['POST'])
-def del_onu():
-    localidade = request.form['serial']
-    serial = request.form['olts']
-    return f"<h1>{serial}--{localidade}</h1>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Usuario ou senha invalida'
+        return render_template('login.html', error=error)
+
+
+@app.route('/remove/<model_prediction>', methods=['POST'])
+def del_onu(model_prediction):
+    return render_template(
+        'home.html',
+        prediction=model_prediction,
+        show_predictions_modal=True
+    )
 
 
 @app.route('/selremove/', methods=['GET', 'POST'])
 def get_localidades():
     if request.method == 'GET':
+        print('get')
         localidades = list(Localidade.select().dicts())
         return render_template('onu-remove.html', my_string="Include Help!", my_list=localidades)
-
-@app.route('/sucess')
-def sucess():
-    return "<h1>Admin</h1>"
-
-
-@app.route('/guest/<guest>')
-def guest(guest):
-    return f'<p>Olá guest <h>{guest}</h></p>'
-
-
-@app.route('/user/')
-@app.route('/user/<name>')
-def user(name=None):
-    if name is None:
-        return 'user not found'
-
-    if name == 'admin':
-        return redirect(url_for('admin'))
     else:
-        return redirect(url_for('guest', guest=name))
+        print('post')
 
 
 if __name__ == '__main__':
