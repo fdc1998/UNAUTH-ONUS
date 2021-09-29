9f2f7c572a8d22c6b27e364d0179f28f1b508806
Flavio Fernandes Vasconcelos <fdc1998@gmail.com>
2021-09-29 15:10:00 -0300

    29-09-21

--- DEPLOY/models/config.py
+++
@@ -1,5 +0,0 @@
-
-def config():
-    host = '127.0.0.1'
-    port = 5000
-    return host, port
--- DEPLOY/models/initdb.py
+++
@@ -1,6 +0,0 @@
-from models.connections import *
-from models.user_model import Olt2location, Olt, Location, Users
-
-
-db = dbconnect()
-db.create_tables([Location, Olt, Users, Olt2location])
