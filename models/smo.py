087f94de39fa6eb265004971dc013341a73b52a0
Flavio Fernandes Vasconcelos <fdc1998@gmail.com>
2021-12-08 14:23:08 -0300

    smo

--- models/smo.py
+++ models/smo.py
@@ -3,14 +3,14 @@ import time
 
 def find_onu_id(serial):
     r = requests.get(f'http://172.16.254.13:8080/smo/api/onus/?serial={serial}')
-    if r.status_code == 200:
+    if r.status_code == 200 and len(r.json()) != 0:
         return r.json()[0]['id']
     else:
         return 'Error'
 
 def remove_onu(id):
     r = requests.post(f'http://172.16.254.13:8080/smo/api/onus/{id}/')
-    if requests.status_code == 200:
+    if r.status_code == 200:
         return 'OK'
     else:
         return 'Error'
